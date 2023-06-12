7725de87306b1b22d76cc9323a4c9de9d27e8a08
christiankasegne <christiankasegne@gmail.com>
2023-06-12 08:31:07 -0400

    updated server folder by adding and deleting file

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema } = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
@@ -19,12 +19,18 @@ const bookSchema = new Schema({
   image: {
     type: String,
   },
-  link: {
-    type: String,
-  },
   title: {
     type: String,
     required: true,
+  },
+  infoLink: {
+    type: String,
+  },
+  previewLink: {
+    type: String,
+  },
+  publishedDate: {
+    type: String,
   },
 });
 
